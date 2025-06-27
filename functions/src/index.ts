b3ce772d720ccef5ddf9210e68283dfbe91f511f
Abraham Williams <4braham@gmail.com>
2025-06-27 18:33:37 -0500

    add layout with user info

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,7 @@ import * as logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as catAPI from './cat-api';
+// import * as catAPI from './cat-api';
 
 const API_KEY = process.env.UNSPLASH_CLIENT_ID;
 
@@ -29,7 +29,8 @@ export const cat = onRequest(
     }
 
     try {
-      const cat = await catAPI.get({ clientId: API_KEY });
+      const cat = JSON.parse(await fs.promises.readFile(path.join(__dirname, '..', 'test', 'fixtures', 'photo.json'), 'utf8'));
+      // const cat = await catAPI.get({ clientId: API_KEY });
       logger.log('Cat fetched successfully:', cat);
 
       const templatePath = path.join(__dirname, '..', 'template.html');
@@ -37,7 +38,10 @@ export const cat = onRequest(
 
       const html = htmlTemplate
         .replace('{{LINK_URL}}', cat.links.html)
-        .replace('{{IMAGE_URL}}', cat.urls.full);
+        .replace('{{IMAGE_URL}}', cat.urls.full)
+        .replace('{{USER_PROFILE_IMAGE}}', cat.user.profile_image.medium)
+        .replace('{{USER_NAME}}', cat.user.name)
+        .replace('{{USER_USERNAME}}', cat.user.username);
 
       response.set('Cache-Control', 'public, max-age=1, s-maxage=1');
       response.status(200);
