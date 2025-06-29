2b81b318e446b26ad7cb3924a9d263493df65f3f
Abraham Williams <4braham@gmail.com>
2025-06-28 19:23:43 -0500

    add fb to web

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 console.log('Daily Cat app initialized');
+
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
+fetch('/__/firebase/init.json').then(async (response) => {
+  const app = initializeApp(await response.json());
+  const messaging = getMessaging(app);
+  console.log('Firebase app initialized:', app.name);
+  console.log('Firebase Messaging initialized:', messaging);
+});
